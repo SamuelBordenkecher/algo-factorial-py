b21317722d2a459a754c17bf87497c938b6a4f68
Samuel Bordenkecher <stb4380@gmail.com>
2025-10-09 15:13:45 -0400

    completed Factorial.py

--- factorial.py
+++ factorial.py
@@ -1,3 +1,15 @@
 def factorial(num):
-	# your code here
-	pass
+	if num == 0:
+		return 1
+	elif num == 1:
+		return 1
+	elif num > 1:
+		total = 1
+		set_num = num
+		while set_num > 0:
+			total *= set_num
+			set_num -= 1
+	return total
+
+
+print(factorial(4))
